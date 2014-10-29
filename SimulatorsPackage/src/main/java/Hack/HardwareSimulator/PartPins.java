de50c3dad811c47367de2bae91af5b84ec580c52
Ivan Toshkov <ivan@toshkov.org>
2014-10-29 15:27:38 +0200

    Don't use 'enum' as variable name

--- SimulatorsPackage/src/main/java/Hack/HardwareSimulator/PartPins.java
+++ SimulatorsPackage/src/main/java/Hack/HardwareSimulator/PartPins.java
@@ -65,9 +65,9 @@ public class PartPins extends ValueComputerPart {
         partGateClass = null;
 
         // remove all node gui adapters
-        Enumeration enum = nodes.keys();
-        while (enum.hasMoreElements()) {
-            Node node = (Node)enum.nextElement();
+        Enumeration en = nodes.keys();
+        while (en.hasMoreElements()) {
+            Node node = (Node)en.nextElement();
             Node nodeAdapter = (Node)nodes.get(node);
             node.removeListener(nodeAdapter);
         }
