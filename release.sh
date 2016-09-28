731f38df554a1f6806d992ada39a316ee5bd5706
Ivan Toshkov <ivan@toshkov.org>
2016-09-28 11:10:00 +0300

    Leave just the software suite

--- release.sh
+++ release.sh
@@ -2,6 +2,7 @@
 
 #set -x
 
+DIST_NAME=n2t-software-suite
 RELEASE_DIR=Release
 
 cd $(dirname $0)
@@ -11,17 +12,17 @@ rm -rf $RELEASE_DIR
 mvn clean package
 
 DIR=$(mktemp -d)
-INST_DIR="${DIR}/nand2tetris/tools/bin"
+INST_DIR="${DIR}/${DIST_NAME}/bin"
 
-git archive --format=tar HEAD nand2tetris | tar -x -C ${DIR}
+git archive --format=tar HEAD ${DIST_NAME} | tar -x -C ${DIR}
 
 cp HardwareSimulator/target/HardwareSimulator-*.jar $INST_DIR/HardwareSimulator.jar
 cp CPUEmulator/target/CPUEmulator-*.jar $INST_DIR/CPUEmulator.jar
 cp VMEmulator/target/VMEmulator-*.jar $INST_DIR/VMEmulator.jar
 
 pushd $DIR
-zip -r nand2tetris.zip nand2tetris/
+zip -r ${DIST_NAME}.zip ${DIST_NAME}
 popd
 mkdir $RELEASE_DIR
-mv $DIR/nand2tetris.zip $RELEASE_DIR
+mv $DIR/${DIST_NAME}.zip $RELEASE_DIR
 rm -rf $DIR
